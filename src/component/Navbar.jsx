1f9aec03f6f761fd7f7a4e9bc6c93fc78fa36f90
SelasiMadeItHere <selasiprecious20@gmail.com>
2022-10-09 02:28:06 +0000

    Push 1

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+import MicrophoneIcon from '@heroicons/react/outline'
+
+
+const Navbar = ()=> {
+  return (
+    <div className=' text-4xl'>
+        Busansga
+        <MenuIcon  className=' w-xl'/>
+        <MicrophoneIcon />
+    </div>
+  )
+}
+
+export default Navbar
